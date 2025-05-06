f0e92cf2531701b43bb9611f75bb41a045b06b8b
Koen Reinders <koenf.reinders@gmail.com>
2025-05-06 13:41:58 +0200

    Refactor compute_summary statistics

--- R/summary_statistics.R
+++ R/summary_statistics.R
@@ -18,20 +18,10 @@ safe_var <- function(x, default = 0) {
 # Compute Summary Statistics                                                   #
 #------------------------------------------------------------------------------#
 
-compute_summary_statistics <- function(SimulationSingle, nosoi_settings) {
-  Data <- SimulationSingle
-  All_Data_A <- getTableHosts(Data)
-  Data_A <- All_Data_A[, c("hosts.ID", "inf.by")]
-
-  # Ensure Data_A exists
-  if (is.null(Data_A) || nrow(Data_A) == 0) {
-    warning("No host data found in simulation.")
-    return(data.table(seed = NA))  # Return a placeholder to prevent errors
-  }
-
+compute_summary_statistics <- function(sim_result, hosts_table, nosoi_settings) {
   # Basic Statistics
-  ss_noninf <- sum(!Data_A$hosts.ID %in% Data_A$inf.by)  # Number of non-infecting hosts
-  frequency_table <- table(Data_A$inf.by)
+  ss_noninf <- sum(!hosts_table$hosts.ID %in% hosts_table$inf.by)  # Number of non-infecting hosts
+  frequency_table <- table(hosts_table$inf.by)
   result_table <- data.frame(hosts.ID = names(frequency_table), Frequency = as.numeric(frequency_table))
 
   ss_mean_secinf <- safe_mean(result_table$Frequency)
@@ -43,36 +33,36 @@ compute_summary_statistics <- function(SimulationSingle, nosoi_settings) {
   result_table$Cumulative <- cumsum(result_table$Frequency)
   ss_fractop50 <- which(result_table$Cumulative >= (0.5 * sum(result_table$Frequency)))[1] / length(result_table$Frequency)
 
-  ss_hostspertime <- length(Data_A$hosts.ID) / Data$total.time  # Hosts per time
+  ss_hostspertime <- length(hosts_table$hosts.ID) / sim_result$total.time  # Hosts per time
 
   # Infection Time Statistics
-  inf_time <- All_Data_A$out.time - All_Data_A$inf.time
+  inf_time <- hosts_table$out.time - hosts_table$inf.time
   inf_time <- inf_time[!is.na(inf_time)]
 
   ss_mean_inftime <- safe_mean(inf_time)
   ss_med_inftime <- safe_median(inf_time)
   ss_var_inftime <- safe_var(inf_time)
 
-  ss_prop_infectors <- length(frequency_table) / length(All_Data_A$hosts.ID)  # Proportion of infecting hosts
+  ss_prop_infectors <- length(frequency_table) / length(hosts_table$hosts.ID)  # Proportion of infecting hosts
 
-  ss_active_final <- sum(All_Data_A$active)  # Number of active hosts at the end
-  ss_hosts_total <- length(All_Data_A$hosts.ID)  # Total hosts over the simulation
+  ss_active_final <- sum(hosts_table$active)  # Number of active hosts at the end
+  ss_hosts_total <- length(hosts_table$hosts.ID)  # Total hosts over the simulation
   ss_frac_active_final <- ifelse(ss_hosts_total != 0, ss_active_final / ss_hosts_total, 0)  # Ratio of active hosts to total
 
   # Infection Timing Metrics
-  result_td <- merge(All_Data_A, All_Data_A, by.x = "inf.by", by.y = "hosts.ID", suffixes = c("", "_infected_by"))
+  result_td <- merge(hosts_table, hosts_table, by.x = "inf.by", by.y = "hosts.ID", suffixes = c("", "_infected_by"))
   result_td$inf_time_diff <- result_td$inf.time - result_td$inf.time_infected_by
   result_td <- result_td[, c("hosts.ID", "inf.by", "inf.time", "inf.time_infected_by", "inf_time_diff")]
 
-  ss_mean_inflag <- safe_mean(result_td$inf_time_diff, Data$total.time + 1)
-  ss_min_inflag <- ifelse(nrow(result_td) <= 1, Data$total.time + 1, min(aggregate(inf_time_diff ~ inf.by, result_td, min)$inf_time_diff))
+  ss_mean_inflag <- safe_mean(result_td$inf_time_diff, sim_result$total.time + 1)
+  ss_min_inflag <- ifelse(nrow(result_td) <= 1, sim_result$total.time + 1, min(aggregate(inf_time_diff ~ inf.by, result_td, min)$inf_time_diff))
   ss_med_inflag <- safe_median(result_td$inf_time_diff)
   ss_var_inflag <- safe_var(result_td$inf_time_diff)
 
-  ss_frac_runtime <- Data$total.time / nosoi_settings$length  # Ratio of simulation run time to max time
+  ss_frac_runtime <- sim_result$total.time / nosoi_settings$length  # Ratio of simulation run time to max time
 
   # Network Structure Analysis
-  edges <- All_Data_A[, c("inf.by", "hosts.ID")]
+  edges <- hosts_table[, c("inf.by", "hosts.ID")]
   graph <- graph_from_data_frame(edges, directed = TRUE)
 
   ss_g_degree <- safe_mean(degree(graph))                   # Degree distribution
@@ -85,15 +75,15 @@ compute_summary_statistics <- function(SimulationSingle, nosoi_settings) {
   ss_g_effglob <- global_efficiency(graph)                  # Network global efficiency
 
   # Mortality-related statistics from host fate
-  if ("fate" %in% colnames(All_Data_A)) {
+  if ("fate" %in% colnames(hosts_table)) {
     # Mapping:
     #   0 -> still active
     #   1 -> deceased
     #   2 -> recovered
-    death_mask <- All_Data_A$fate == 1
-    recovery_mask <- All_Data_A$fate == 2
+    death_mask <- hosts_table$fate == 1
+    recovery_mask <- hosts_table$fate == 2
 
-    time_to_death <- All_Data_A$out.time[death_mask] - All_Data_A$inf.time[death_mask]
+    time_to_death <- hosts_table$out.time[death_mask] - hosts_table$inf.time[death_mask]
 
     ss_deaths <- sum(death_mask)                            # Total no. deaths
     ss_mean_deaths <- safe_mean(death_mask)                 # Proportion of hosts that died
