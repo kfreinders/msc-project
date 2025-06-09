65a02472f203cb308b73e9c6f9ae2afc7004a9a6
Koen Reinders <koenf.reinders@gmail.com>
2025-06-09 02:56:09 +0200

    Tune SLURM resource allocation for scarce data generation

--- batch_scripts/create_scarce_data.sh
+++ batch_scripts/create_scarce_data.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=create_scarce_data
 #SBATCH --time=02-00:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=4GB
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8GB
 #SBATCH --output=create_scarce_data_%j.out
 #SBATCH --error=create_scarce_data_%j.err
 
