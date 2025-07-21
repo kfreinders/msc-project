fe62675d0899ca642948465aebea045a478a7a26
Koen Reinders <koenf.reinders@gmail.com>
2025-07-22 00:01:26 +0200

    refactor: search from root path instead of hardcoding all file paths

--- python/analysis/predicted_vs_true.py
+++ python/analysis/predicted_vs_true.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 from pathlib import Path
 import torch
+import json
 
 from models.tuning import HyperParams, model_factory
 from dataproc.nosoi_split import NosoiSplit
@@ -11,44 +12,43 @@ from utils.utils import plot_predictions, predict_nosoi_parameters
 
 def main() -> None:
     # Set up logger
-    setup_logging("training")
+    setup_logging("predicted_vs_true")
     logger = logging.getLogger(__name__)
 
     # Use CUDA if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info(f"Using device: {device}")
 
-    # Load data splits from disk
-    splits_path = Path("data/splits/scarce_0.00")
-    train_split = NosoiSplit.load("train", splits_path)
-    test_split = NosoiSplit.load("test", splits_path)
-    logger.info(f"Loaded saved data splits from {splits_path}")
+    # Set root path
+    root_path = Path("data/splits/scarce_0.00")
 
-    cfg = HyperParams(
-        learning_rate=0.0005186374528320235,
-        hidden_size=256,
-        num_layers=2,
-        dropout_rate=0.10448580769582116,
-        batch_size=16
-    )
+    test_split = NosoiSplit.load("test", root_path)
+    logger.info(f"Loaded saved data splits from {root_path}")
+
+    # Retrieve hyperparameters
+    with open(root_path / "best_config.json") as handle:
+        data = json.load(handle)
+        cfg = HyperParams.from_dict(data)
+
     logger.info(f"HyperParams: {cfg}")
 
+    # Set up model architecture
     model = model_factory(
-        train_split.input_dim,
-        train_split.output_dim,
+        test_split.input_dim,
+        test_split.output_dim,
         cfg,
         device
     )
 
     # Load model (redundant in case since already in memory)
-    model_path = Path("data/dnn/scarce_0.00/regressor.pt")
+    model_path = root_path / "regressor.pt"
     logger.info(f"Loading saved model from: {model_path}")
     model.load_state_dict(
         torch.load(model_path, map_location=device)
     )
     model.to(device)
 
-    logger.info(f"Making predictions...")
+    logger.info("Making predictions...")
     test_loader = test_split.make_dataloader(cfg.batch_size)
     preds, trues = predict_nosoi_parameters(model, test_loader, device)
 
