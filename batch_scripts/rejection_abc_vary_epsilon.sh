2006e4d3e7a74097c658178fb2a5493f3c32184b
Koen Reinders <koenf.reinders@gmail.com>
2025-08-21 19:09:38 +0200

    fix: separator formatting in batch rejection ABC script

--- batch_scripts/rejection_abc_vary_epsilon.sh
+++ batch_scripts/rejection_abc_vary_epsilon.sh
@@ -2,5 +2,9 @@
 
 for i in $(seq 0.2 0.2 2.0)
 do
+    echo "--------------------------------------------------------------------------------"
+    echo " epsilon = $i"
+    echo "--------------------------------------------------------------------------------"
     PYTHONPATH=python python3 -m analysis.abc --method rejection --epsilon "$i"
+    echo
 done
